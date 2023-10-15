a31e942f7e5cbee7a57d22d54ede42540c8dee68
SoFranci <trevisanfrancesco022@gmail.com>
2023-10-15 09:52:12 +0200

    Fixes settings iPad support

--- waifu2D/SettingsView.swift
+++ waifu2D/SettingsView.swift
@@ -8,86 +8,100 @@ struct SettingsView: View {
     @Binding var isButtonEnabled: Bool
     @Binding var isPasscodeEnabled: Bool
     
-    
     let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "N/A"
     let appBuild = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "N/A"
     
     var body: some View {
+        #if os(iOS)
         NavigationView {
-            NavigationLink(destination: EmptyView(), isActive: $isSettingsVisible) {
-                EmptyView()
+            content
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        #else
+        HStack {
+            NavigationView {
+                List {
+                    settingsList
+                }
+                .navigationBarItems(leading: Button("Close") {
+                    isSettingsVisible.toggle()
+                })
+                .navigationTitle("Settings")
             }
-            .hidden()
+            .frame(width: 300)
             
-            Form {
-                Section(header: Text("General"), footer: Text("If you enable/disable 'Use Buttons' you need to restart the app to see them.")) {
-                    
-                    Toggle(isOn: $isHaptic2Enabled, label: {
-                        Text("Haptic Feedback")
-                    })
+            content
+        }
+        #endif
+    }
+    
+    var content: some View {
+        Form {
+            Section(header: Text("General"), footer: Text("If you enable/disable 'Use Buttons' you need to restart the app to see them.")) {
+                Toggle(isOn: $isHaptic2Enabled, label: {
+                    Text("Vibration Feedback")
+                })
                 
-                    Toggle(isOn: $isButtonEnabled, label: {
-                        Text("Use Buttons")
-                    })
-                    
-                    Toggle(isOn: $isGestureEnabled, label: {
-                        Text("Use Gestures")
-                    })
-                    
-                }
+                Toggle(isOn: $isButtonEnabled, label: {
+                    Text("Use Buttons")
+                })
                 
-                Section(header: Text("Security"), footer: Text("If no Biometric Authentication is enabled on the device the passcode will be asked.")) {
-                    
-                    Toggle(isOn: $isPasscodeEnabled, label: {
-                        Text("Biotmetric Authentication")
-                    })
-                }
-                
-                
-                Section {
-                    
-                    HStack {
-                        Text("Submit Waifu")
-                    }
-                    .font(.system(size: 15, weight: .regular))
-                    .onTapGesture {
-                        if let url = URL(string: "https://cranci1.github.io/myWaifu2D/submit.html") {
-                            UIApplication.shared.open(url)
-                        }
-                    }
-                }
-                
-                
-                Section(header: Text("App Info")) {
-                        HStack {
-                        Text("Version: \(appVersion)")
-                        }
-                        HStack {
-                        Text("Build: \(appBuild)")
-                        }
-                }
-                
+                Toggle(isOn: $isGestureEnabled, label: {
+                    Text("Use Gestures")
+                })
             }
-            .navigationBarBackButtonHidden(true)
-            .navigationTitle("Settings")
             
-            Button("Toggle Settings") {
-                isSettingsVisible.toggle()
+            Section(header: Text("Security"), footer: Text("If no Biometric Authentication is enabled on the device the passcode will be asked.")) {
+                Toggle(isOn: $isPasscodeEnabled, label: {
+                    Text("Biometric Authentication")
+                })
+            }
+            
+            Section {
+                HStack {
+                    Text("Submit Waifu")
+                }
+                .font(.system(size: 15, weight: .regular))
+                .onTapGesture {
+                    if let url = URL(string: "https://cranci1.github.io/myWaifu2D/submit.html") {
+                        UIApplication.shared.open(url)
+                    }
+                }
+            }
+            
+            Section(header: Text("App Info")) {
+                HStack {
+                    Text("Version: \(appVersion)")
+                }
+                HStack {
+                    Text("Build: \(appBuild)")
+                }
             }
         }
-        
+        .navigationBarBackButtonHidden(true)
+        .navigationTitle("Settings")
+    }
+    
+    var settingsList: some View {
+        Form {
+            Section {
+                NavigationLink(destination: EmptyView()) {
+                    Text("Placeholder for Settings")
+                }
+            }
+        }
+        .navigationBarTitle("Settings")
     }
 }
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView (
-            
             isHaptic2Enabled: .constant(false),
             isGestureEnabled: .constant(true),
             isButtonEnabled: .constant(true),
             isPasscodeEnabled: .constant(false)
         )
-            .preferredColorScheme(.dark)
+        .preferredColorScheme(.dark)
     }
 }
